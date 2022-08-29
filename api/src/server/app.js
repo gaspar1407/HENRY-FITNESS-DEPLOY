bbc011e8a6c704c455d61b772e0955c36f8d948e
gaspar <gasparmunoz@hotmail.com>
2022-08-28 23:01:44 -0300

    cambio de cors

--- api/src/server/app.js
+++ api/src/server/app.js
@@ -3,7 +3,7 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const routes = require("../routes/index.js");
-var cors = require("cors");
+/* var cors = require("cors"); */
 const { CORS_URL } = process.env;
 
 require("../db/db.js");
@@ -12,7 +12,7 @@ const server = express();
 
 server.name = "API";
 
-server.use(cors());
+/* server.use(cors()); */
 server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
